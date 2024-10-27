5f7da13805366694759e789376771c800e5324ed
Mostafa Faouzi <mostafafaouzi16@gmail.com>
2024-10-27 19:31:36 +0100

    first commit

---
+++ EventReservationSystem/src/main/java/com/ENSA/EventReservationSystem/EventReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ENSA.EventReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventReservationSystemApplication.class, args);
+	}
+
+}
